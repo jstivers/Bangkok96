139c7ae470ed8a5272bd3f94f0d1894e3d0d282c
jstivers@usc.edu <jstivers@usc.edu>
2017-07-11 18:06:31 -0700

    Initial Commit

---
+++ Bangkok96/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Bangkok96
+//
+//  Created by John Stivers on 7/11/17.
+//  Copyright © 2017 John Stivers. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
